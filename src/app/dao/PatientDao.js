02f52926beb811b59dc87b5e1dbf86e741a538af
Liangxiaojun <fatalxiao@163.com>
2018-01-16 15:27:16 +0800

    update PatientDAO

--- src/app/dao/PatientDao.js
+++ src/app/dao/PatientDao.js
@@ -2,7 +2,23 @@ const mysql = require('../utils/mysql.js');
 
 async function getPatients() {
 
-    const sql = 'SELECT * FROM patient';
+    const sql =
+        `SELECT
+        id,
+        group_id as groupId,
+        patient_name as patientName,
+        age,
+        gestational_days as gestationalDays,
+        height,
+        weight,
+        initial_vas_score as initialVasScore,
+        cervical_dilation_at_time_of_ea as cervicalDilationAtTimeOfEa,
+        heart_rate as heartRate,
+        systolic_blood_pressure as systolicBloodPressure,
+        diastolic_blood_pressure as diastolicBloodPressure,
+        foetal_heart_rate as foetalHeartRate,
+        description 
+        FROM patient`;
 
     const result = await mysql.execQuery(sql);
 
