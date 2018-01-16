d981c11b2d18416be159a716dffaa6db2e00a62d
Liangxiaojun <fatalxiao@163.com>
2018-01-16 15:28:12 +0800

    update GroupDao

--- src/app/dao/GroupDao.js
+++ src/app/dao/GroupDao.js
@@ -2,7 +2,7 @@ const mysql = require('../utils/mysql.js');
 
 async function getGroups() {
 
-    const sql = 'SELECT * FROM dpe_group';
+    const sql = 'SELECT id, group_name as groupName FROM dpe_group';
 
     const result = await mysql.execQuery(sql);
 
