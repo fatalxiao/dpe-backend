5af5b73aca6a65963ef43055c581293a5827af20
Liangxiaojun <fatalxiao@163.com>
2018-01-17 14:40:07 +0800

    update

--- src/app/model/PatientModel.js
+++ src/app/model/PatientModel.js
@@ -8,19 +8,54 @@ const Sequelize = require('Sequelize'),
             unique: true,
             primaryKey: true
         },
-        group_id: Sequelize.INTEGER(1),
-        patient_name: Sequelize.STRING(30),
-        age: Sequelize.INTEGER(3),
-        gestational_days: Sequelize.INTEGER(3),
-        height: Sequelize.FLOAT,
-        weight: Sequelize.FLOAT,
-        initial_vas_score: Sequelize.INTEGER(3),
-        cervical_dilation_at_time_of_ea: Sequelize.INTEGER(3),
-        heart_rate: Sequelize.INTEGER(3),
-        systolic_blood_pressure: Sequelize.INTEGER(3),
-        diastolic_blood_pressure: Sequelize.INTEGER(3),
-        foetal_heart_rate: Sequelize.INTEGER(3),
-        description: Sequelize.STRING(1000)
+        groupId: {
+            type: Sequelize.INTEGER(1),
+            filed: 'group_id'
+        },
+        patientName: {
+            type: Sequelize.STRING(30),
+            filed: 'patient_name'
+        },
+        age: {
+            type: Sequelize.INTEGER(3)
+        },
+        gestationalDays: {
+            type: Sequelize.INTEGER(3),
+            filed: 'gestational_days'
+        },
+        height: {
+            type: Sequelize.FLOAT
+        },
+        weight: {
+            type: Sequelize.FLOAT
+        },
+        initialVasScore: {
+            type: Sequelize.INTEGER(3),
+            filed: 'initial_vas_score'
+        },
+        cervicalDilationAtTimeOfEa: {
+            type: Sequelize.INTEGER(3),
+            filed: 'cervical_dilation_at_time_of_ea'
+        },
+        heartRate: {
+            type: Sequelize.INTEGER(3),
+            filed: 'heart_rate'
+        },
+        systolicBloodPressure: {
+            type: Sequelize.INTEGER(3),
+            filed: 'systolic_blood_pressure'
+        },
+        diastolicBloodPressure: {
+            type: Sequelize.INTEGER(3),
+            filed: 'diastolic_blood_pressure'
+        },
+        foetalHeartRate: {
+            type: Sequelize.INTEGER(3),
+            filed: 'foetal_heart_rate'
+        },
+        description: {
+            type: Sequelize.STRING(1000)
+        }
     }, {
         timestamps: true,
         createdAt: 'ctime',
