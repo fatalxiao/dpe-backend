b0c06b249245aca376346a6e8605d6be4659add0
Liangxiaojun <fatalxiao@163.com>
2018-01-22 21:24:29 +0800

    update

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Koa from 'koa';
+import Api from './app/utils/Api.js';
+import bodyParser from 'koa-bodyparser';
+import config from './config.js';
+
+const app = new Koa();
+
+app.use(bodyParser());
+app.use(Api.router2controller());
+
+app.listen(config.port);
+console.log(`Server started and listen on port ${config.port}`);
