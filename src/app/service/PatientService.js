5809b8e07583f1c8de53f5fef243e37085b104a6
Liangxiaojun <fatalxiao@163.com>
2018-01-22 14:26:25 +0800

    update

--- src/app/service/PatientService.js
+++ src/app/service/PatientService.js
@@ -6,7 +6,20 @@ async function getPatients() {
 };
 
 async function addPatient(formData) {
+
+    formData.age = formData.age || null;
+    formData.gestationalDays = formData.gestationalDays || null;
+    formData.height = formData.height || null;
+    formData.weight = formData.weight || null;
+    formData.heartRate = formData.heartRate || null;
+    formData.initialVasScore = formData.initialVasScore || null;
+    formData.cervicalDilationAtTimeOfEA = formData.cervicalDilationAtTimeOfEA || null;
+    formData.systolicBloodPressure = formData.systolicBloodPressure || null;
+    formData.diastolicBloodPressure = formData.diastolicBloodPressure || null;
+    formData.foetalHeartRate = formData.foetalHeartRate || null;
+
     return Response.buildSuccess(await PatientDao.addPatient(formData));
+
 };
 
 module.exports = {
