c2009d67b39852455502f30ad301987fdee1629b
Liangxiaojun <fatalxiao@163.com>
2018-01-22 22:56:36 +0800

    update

--- src/app/utils/ApiDecorator.js
+++ src/app/utils/ApiDecorator.js
@@ -21,7 +21,22 @@ function addMapping(router, controllers) {
 
         _.pull(methods, 'name', 'constructor', 'length', 'prototype');
 
-        methods.filter(item => controller[item][API_METHOD] && controller[item][API_ROUTE]);
+        methods
+        //.filter(methodName => controller[methodName][API_METHOD] && controller[methodName][API_ROUTE])
+        .forEach(methodName => {
+
+            const method = controller[methodName][API_METHOD],
+                route = controller[methodName][API_ROUTE];
+
+            if (!method || !route) {
+                return;
+            }
+
+            router[method.toLowerCase()](route, controller[methodName]);
+
+            console.log(`register URL mapping: ${method.toUpperCase()} ${route}`);
+
+        });
 
         // console.log(`register URL mapping: ${controller[API_TYPE].toUpperCase()} ${controller[API_ROUTE]}`);
         // router[controller[API_TYPE]](controller[API_ROUTE], controller);
