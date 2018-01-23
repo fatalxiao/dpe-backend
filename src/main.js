cce123ca614cbc6854591a3fa8a80212d8da2744
Liangxiaojun <fatalxiao@163.com>
2018-01-23 12:57:50 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Koa from 'koa';
-import mappingRouterToController from './app/utils/mappingRouterToController.js';
+import serve from 'koa-static';
 import bodyParser from 'koa-bodyparser';
+
+import mappingRouterToController from './app/utils/mappingRouterToController.js';
 import config from './config.js';
 
 const app = new Koa();
@@ -8,6 +10,7 @@ const app = new Koa();
 app
 .use(bodyParser())
 .use(mappingRouterToController(__dirname))
+.use(serve(__dirname + '/swagger/'))
 .listen(config.port, () => {
     console.log(`Server started and listen on port ${config.port}`);
 });
