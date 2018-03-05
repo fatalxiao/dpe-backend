18755c59d44ded0b3b9bd0d221e0980564884c88
Liangxiaojun <fatalxiao@163.com>
2018-03-05 16:04:23 +0800

    update

--- db/initialData/sensory_block.sql
+++ db/initialData/sensory_block.sql
@@ -6,8 +6,10 @@ INSERT INTO dpe.sensory_block (id, sensory_block_name, sensory_block_value) VALU
 INSERT INTO dpe.sensory_block (id, sensory_block_name, sensory_block_value) VALUES (6, 'L2', 6);
 INSERT INTO dpe.sensory_block (id, sensory_block_name, sensory_block_value) VALUES (7, 'L1', 7);
 INSERT INTO dpe.sensory_block (id, sensory_block_name, sensory_block_value) VALUES (8, 'T12', 8);
-INSERT INTO dpe.sensory_block (id, sensory_block_name, sensory_block_value) VALUES (9, 'T10', 9);
-INSERT INTO dpe.sensory_block (id, sensory_block_name, sensory_block_value) VALUES (10, 'T8', 10);
-INSERT INTO dpe.sensory_block (id, sensory_block_name, sensory_block_value) VALUES (11, 'T6', 11);
-INSERT INTO dpe.sensory_block (id, sensory_block_name, sensory_block_value) VALUES (12, 'T4', 12);
-INSERT INTO dpe.sensory_block (id, sensory_block_name, sensory_block_value) VALUES (13, 'T2', 13);
+INSERT INTO dpe.sensory_block (id, sensory_block_name, sensory_block_value) VALUES (9, 'T11', 9);
+INSERT INTO dpe.sensory_block (id, sensory_block_name, sensory_block_value) VALUES (10, 'T10', 10);
+INSERT INTO dpe.sensory_block (id, sensory_block_name, sensory_block_value) VALUES (11, 'T9', 11);
+INSERT INTO dpe.sensory_block (id, sensory_block_name, sensory_block_value) VALUES (12, 'T8', 12);
+INSERT INTO dpe.sensory_block (id, sensory_block_name, sensory_block_value) VALUES (14, 'T6', 14);
+INSERT INTO dpe.sensory_block (id, sensory_block_name, sensory_block_value) VALUES (16, 'T4', 16);
+INSERT INTO dpe.sensory_block (id, sensory_block_name, sensory_block_value) VALUES (18, 'T2', 18);
