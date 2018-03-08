a6b8c8fdb1bd5dcfd956e794928b80574b91ddc1
Liangxiaojun <fatalxiao@163.com>
2018-03-08 20:44:09 +0800

    update

--- db/createTable/patients.sql
+++ db/createTable/patients.sql
@@ -1,37 +1,39 @@
 CREATE TABLE patients
 (
-  id                              VARCHAR(10)            NOT NULL
+  id                              VARCHAR(10)     NOT NULL
   COMMENT '住院号'
     PRIMARY KEY,
-  group_id                        INT                    NULL
+  group_id                        INT             NULL
   COMMENT '组别',
-  patient_name                    VARCHAR(30)            NULL
+  name                            VARCHAR(30)     NULL
   COMMENT '姓名',
-  age                             INT                    NULL
+  age                             INT             NULL
   COMMENT '年龄',
-  gestational_days                INT                    NULL
+  gestational_days                INT             NULL
   COMMENT '怀孕天数',
-  height                          FLOAT                  NULL
+  height                          FLOAT           NULL
   COMMENT '身高',
-  weight                          FLOAT                  NULL
+  weight                          FLOAT           NULL
   COMMENT '体重',
-  initial_vas_score               INT                    NULL
+  initial_vas_score               INT             NULL
   COMMENT '镇痛前VAS评分',
-  cervical_dilation_at_time_of_ea INT                    NULL
-  COMMENT '镇痛前宫口',
-  heart_rate                      INT                    NULL
+  heart_rate                      INT             NULL
   COMMENT '心率',
-  systolic_blood_pressure         INT                    NULL
+  systolic_blood_pressure         INT             NULL
   COMMENT '收缩压',
-  diastolic_blood_pressure        INT                    NULL
+  diastolic_blood_pressure        INT             NULL
   COMMENT '舒张压',
-  description                     VARCHAR(1000)          NULL
+  cervical_dilation_at_time_of_ea INT             NULL
+  COMMENT '镇痛前宫口',
+  has_oxytocin_at_time_of_ea      TINYINT(1)      NULL
+  COMMENT '镇痛前是否使用催产素',
+  description                     VARCHAR(1000)   NULL
   COMMENT '备注',
-  disabled                        TINYINT(1) DEFAULT '0' NULL
-  COMMENT '是否禁用',
-  utime                           DATETIME               NULL,
-  dtime                           DATETIME               NULL,
-  ctime                           DATETIME               NULL,
+  status                          INT DEFAULT '1' NULL
+  COMMENT '状态，1：启用，0：禁用',
+  utime                           DATETIME        NULL,
+  dtime                           DATETIME        NULL,
+  ctime                           DATETIME        NULL,
   CONSTRAINT patient_id_uindex
   UNIQUE (id)
 )
