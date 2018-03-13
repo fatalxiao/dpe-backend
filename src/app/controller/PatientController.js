da05281ed26928405b5644383b5956132c0f9432
Liangxiaojun <fatalxiao@163.com>
2018-03-13 15:11:21 +0800

    update

--- src/app/controller/PatientController.js
+++ src/app/controller/PatientController.js
@@ -51,13 +51,15 @@ class PatientController {
     @ApiOperation({value: 'export patients', notes: ''})
     static async exportPatients(ctx) {
 
-        const data = [[1, 2, 3], [true, false, null, 'sheetjs'],
-            ['foo', 'bar', new Date('2014-02-19T14:30Z'), '0.3'], ['baz', null, 'qux']];
+        // const data = [[1, 2, 3], [true, false, null, 'sheetjs'],
+        //     ['foo', 'bar', new Date('2014-02-19T14:30Z'), '0.3'], ['baz', null, 'qux']];
+
+        const data = await PatientService.exportPatients();
 
         ctx.set('Content-Type', 'application/vnd.openxmlformats');
         ctx.set('Content-Disposition', `attachment; filename=dpe-data.xlsx`);
 
-        ctx.response.body = xlsx.build([{name: 'mySheetName', data: data}]);
+        ctx.response.body = xlsx.build([{name: 'DPE Data', data: data}]);
 
     }
 
