bd3ff63586a0df785631f3ebca50de65ea669178
liangxiaojun <fatalxiao@163.com>
2018-04-18 08:08:17 +0800

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+process.env.NODE_ENV = '"release"';
+
+var gulp = require('gulp'),
+    babel = require('gulp-babel'),
+    gulpSequence = require('gulp-sequence');
+
+gulp.task('es', () =>
+    gulp.src(['./src/**', '!./src/swagger/**'])
+    .pipe(babel({
+        plugins: ['transform-runtime']
+    }))
+    .pipe(gulp.dest('./dist'))
+);
+
+gulp.task('swagger', () =>
+    gulp.src('./src/swagger/**')
+    .pipe(gulp.dest('./dist/swagger'))
+);
+
+gulp.task('package', gulpSequence('es', 'swagger'));
