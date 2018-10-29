c8ef03efad108fc717d2cf2923ddb85b49c4c2e8
liangxiaojun <fatalxiao@163.com>
2018-10-29 20:57:12 +0800

    add observal_data.sql

--- db/createTable/observal_data.sql
+++ db/createTable/observal_data.sql
@@ -1,109 +1,113 @@
-CREATE TABLE observal_data
+create table observal_data
 (
-  id                                         INT AUTO_INCREMENT
-    PRIMARY KEY,
-  patient_id                                 VARCHAR(10)            NULL
-  COMMENT '孕妇住院号',
-  initial_time                               DATETIME               NULL
-  COMMENT '麻醉开始时间',
-  test_dose                                  INT                    NULL
-  COMMENT '试验剂量(ml)',
-  initial_dose                               INT                    NULL
-  COMMENT '负荷剂量(ml)',
-  pump_consumption                           INT                    NULL
-  COMMENT '泵消耗(ml)',
-  bolus                                      INT                    NULL
-  COMMENT '人工负荷量(ml)',
-  has_carbetocin                             TINYINT(1) DEFAULT '0' NULL
-  COMMENT '是否使用巧特欣',
-  has_hemabate                               TINYINT(1) DEFAULT '0' NULL
-  COMMENT '是否使用欣母沛',
-  pca_count                                  INT                    NULL
-  COMMENT 'PCA次数',
-  manual_bolus_count                         INT                    NULL
-  COMMENT '人工硬膜外追加次数',
-  first_pca_time                             DATETIME               NULL
-  COMMENT '首次PCA时间',
-  duration_of_first_stage_of_labor           INT                    NULL
-  COMMENT '第一产程时长',
-  duration_of_second_stage_of_labor          INT                    NULL
-  COMMENT '第二产程时长',
-  blood_lose                                 INT                    NULL
-  COMMENT '总出血量',
-  has_epidural_catheter_adjuestment          TINYINT(1) DEFAULT '0' NULL
-  COMMENT '是否有硬膜外导管调整',
-  has_epidural_catheter_replacement          TINYINT(1) DEFAULT '0' NULL
-  COMMENT '是否有硬膜外导管重置',
-  has_prenatal_fever                         TINYINT(1) DEFAULT '0' NULL
-  COMMENT '是否有产前发热',
-  has_vasoactive_agent                       TINYINT(1) DEFAULT '0' NULL
-  COMMENT '是否使用血管活性药物',
-  is_unabled_to_puncture_dura                TINYINT(1) DEFAULT '0' NULL
-  COMMENT '是否穿刺未见脑脊液',
-  first_manual_bolus_time                    DATETIME               NULL
-  COMMENT '首次人工硬膜外追加时间',
-  has_nausea                                 TINYINT(1) DEFAULT '0' NULL
-  COMMENT '是否有恶心',
-  has_vomit                                  TINYINT(1) DEFAULT '0' NULL
-  COMMENT '是否有呕吐',
-  has_pruritus                               TINYINT(1) DEFAULT '0' NULL
-  COMMENT '是否有瘙痒',
-  has_hypotension                            TINYINT(1) DEFAULT '0' NULL
-  COMMENT '是否有血压过低',
-  has_caesarean_section                      TINYINT(1) DEFAULT '0' NULL
-  COMMENT '是否有剖宫产',
-  has_instrumental                           TINYINT(1) DEFAULT '0' NULL
-  COMMENT '是否有器械助产',
-  has_postdural_puncture_headache            TINYINT(1) DEFAULT '0' NULL
-  COMMENT '是否有硬脊膜穿破后头痛',
-  has_back_pain                              TINYINT(1) DEFAULT '0' NULL
-  COMMENT '是否有背痛',
-  has_paresthesia                            TINYINT(1) DEFAULT '0' NULL
-  COMMENT '是否有感觉异常',
-  is_iv_epidural_catheter_insertion          TINYINT(1) DEFAULT '0' NULL
-  COMMENT '硬膜外导管是否入血管',
-  is_intrathecal_epidural_catheter_insertion TINYINT(1) DEFAULT '0' NULL
-  COMMENT '是否蛛网膜下腔置管',
-  patient_satisfaction_score                 INT                    NULL
-  COMMENT '孕妇满意度',
-  has_accidental_dural_punture               TINYINT(1)             NULL
-  COMMENT '是否有意外硬脊膜穿破',
-  lateral_episiotomy_vas_score               INT                    NULL
-  COMMENT '侧切时VAS评分',
-  has_lateral_episiotomy                     TINYINT(1) DEFAULT '0' NULL
-  COMMENT '是否有侧切',
-  birth_time                                 DATETIME               NULL
-  COMMENT '分娩时间',
-  foetal_gender                              INT                    NULL
-  COMMENT '新生儿性别，1：男，2：女',
-  foetal_height                              INT                    NULL
-  COMMENT '胎儿身长',
-  foetal_weight                              INT                    NULL
-  COMMENT '胎儿体重',
-  one_minute_apgar_score                     INT                    NULL
-  COMMENT '1分钟APGAR评分',
-  five_minute_apgar_score                    INT                    NULL
-  COMMENT '5分钟APGAR评分',
-  has_nicu                                   TINYINT(1) DEFAULT '0' NULL
-  COMMENT '是否去NICU',
-  nicu_reason                                VARCHAR(255)           NULL
-  COMMENT '去NICU的原因',
-  arterial_ph                                FLOAT                  NULL
-  COMMENT '动脉PH值',
-  arterial_be                                FLOAT                  NULL
-  COMMENT '动脉BE值',
-  venous_ph                                  FLOAT                  NULL
-  COMMENT '静脉PH值',
-  venous_be                                  FLOAT                  NULL
-  COMMENT '静脉BE值',
-  description                                VARCHAR(1000)          NULL
-  COMMENT '备注',
-  ctime                                      DATETIME               NULL,
-  utime                                      DATETIME               NULL,
-  dtime                                      DATETIME               NULL,
-  CONSTRAINT observal_data_id_uindex
-  UNIQUE (id)
+  id                                         int auto_increment,
+  patient_id                                 varchar(10)            null
+  comment '孕妇住院号',
+  initial_time                               datetime               null
+  comment '麻醉开始时间',
+  test_dose                                  int                    null
+  comment '试验剂量(ml)',
+  initial_dose                               int                    null
+  comment '负荷剂量(ml)',
+  pump_consumption                           int                    null
+  comment '泵消耗(ml)',
+  bolus                                      int                    null
+  comment '人工负荷量(ml)',
+  has_carbetocin                             tinyint(1) default '0' null
+  comment '是否使用巧特欣',
+  has_hemabate                               tinyint(1) default '0' null
+  comment '是否使用欣母沛',
+  pca_count                                  int                    null
+  comment 'PCA次数',
+  manual_bolus_count                         int                    null
+  comment '人工硬膜外追加次数',
+  first_pca_time                             datetime               null
+  comment '首次PCA时间',
+  duration_of_first_stage_of_labor           int                    null
+  comment '第一产程时长',
+  duration_of_second_stage_of_labor          int                    null
+  comment '第二产程时长',
+  blood_lose                                 int                    null
+  comment '总出血量',
+  has_epidural_catheter_adjuestment          tinyint(1) default '0' null
+  comment '是否有硬膜外导管调整',
+  has_epidural_catheter_replacement          tinyint(1) default '0' null
+  comment '是否有硬膜外导管重置',
+  has_prenatal_fever                         tinyint(1) default '0' null
+  comment '是否有产前发热',
+  prenatal_fever_temperature                 int                    null
+  comment '产前发热体温',
+  has_vasoactive_agent                       tinyint(1) default '0' null
+  comment '是否使用血管活性药物',
+  is_unabled_to_puncture_dura                tinyint(1) default '0' null
+  comment '是否穿刺未见脑脊液',
+  first_manual_bolus_time                    datetime               null
+  comment '首次人工硬膜外追加时间',
+  has_nausea                                 tinyint(1) default '0' null
+  comment '是否有恶心',
+  has_vomit                                  tinyint(1) default '0' null
+  comment '是否有呕吐',
+  has_pruritus                               tinyint(1) default '0' null
+  comment '是否有瘙痒',
+  has_hypotension                            tinyint(1) default '0' null
+  comment '是否有血压过低',
+  has_caesarean_section                      tinyint(1) default '0' null
+  comment '是否有剖宫产',
+  has_instrumental                           tinyint(1) default '0' null
+  comment '是否有器械助产',
+  has_postdural_puncture_headache            tinyint(1) default '0' null
+  comment '是否有硬脊膜穿破后头痛',
+  has_back_pain                              tinyint(1) default '0' null
+  comment '是否有背痛',
+  has_paresthesia                            tinyint(1) default '0' null
+  comment '是否有感觉异常',
+  is_iv_epidural_catheter_insertion          tinyint(1) default '0' null
+  comment '硬膜外导管是否入血管',
+  is_intrathecal_epidural_catheter_insertion tinyint(1) default '0' null
+  comment '是否蛛网膜下腔置管',
+  patient_satisfaction_score                 int                    null
+  comment '孕妇满意度',
+  has_accidental_dural_punture               tinyint(1)             null
+  comment '是否有意外硬脊膜穿破',
+  lateral_episiotomy_vas_score               int                    null
+  comment '侧切时VAS评分',
+  has_lateral_episiotomy                     tinyint(1) default '0' null
+  comment '是否有侧切',
+  birth_time                                 datetime               null
+  comment '分娩时间',
+  foetal_gender                              int                    null
+  comment '新生儿性别，1：男，2：女',
+  foetal_height                              int                    null
+  comment '胎儿身长',
+  foetal_weight                              int                    null
+  comment '胎儿体重',
+  one_minute_apgar_score                     int                    null
+  comment '1分钟APGAR评分',
+  five_minute_apgar_score                    int                    null
+  comment '5分钟APGAR评分',
+  has_nicu                                   tinyint(1) default '0' null
+  comment '是否去NICU',
+  nicu_reason                                varchar(255)           null
+  comment '去NICU的原因',
+  arterial_ph                                float                  null
+  comment '动脉PH值',
+  arterial_be                                float                  null
+  comment '动脉BE值',
+  venous_ph                                  float                  null
+  comment '静脉PH值',
+  venous_be                                  float                  null
+  comment '静脉BE值',
+  description                                varchar(1000)          null
+  comment '备注',
+  ctime                                      datetime               null,
+  utime                                      datetime               null,
+  dtime                                      datetime               null,
+  constraint observal_data_id_uindex
+  unique (id)
 )
-  COMMENT '观察数据'
-  ENGINE = InnoDB
-  CHARSET = utf8;
+  comment '观察数据'
+  charset = utf8;
+
+alter table observal_data
+  add primary key (id);
+
